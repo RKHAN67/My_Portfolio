80a09d9700e73c07e1a338e1bfa15de4e8c3b193
Rizwan <rizwan819101@gmail.com>
2025-07-07 14:56:15 -0700

    desgin the banner 60% complete

--- app/components/banner/BannerWrapper.jsx
+++ app/components/banner/BannerWrapper.jsx
@@ -1,7 +1,10 @@
-export const BannerWrapper = () => {
+export const BannerWrapper = ({ children, className = "" }) => {
   return (
-    <div className="bg-[linear-gradient(90deg,#0236A6_0%,#637AC8_100%)] rounded-[20px] py-20">
-      {/* content */}
+    <div
+      className={`relative px-[85px] py-[24px] pb-[41px] bg-[linear-gradient(90deg,#0236A6_0%,#637AC8_100%)] rounded-[20px] overflow-hidden ${className}`}
+    >
+      <img src="/assets/images/others/banner-wave.png" alt="image of wave line" className="absolute left-[45%] h-[349px] top-0" />
+      {children}
     </div>
   );
 };
